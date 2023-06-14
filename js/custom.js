199659cc3c62519d842bce75b6ad7fc5473ec2cf
desipossa <c1jang@hanmail.net>
2023-06-14 12:21:07 +0900

    001

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+$(function () {
+
+    $(window).on('scroll', function () {
+        let sct = $(window).scrollTop();
+        sct > 0
+            ? $('.header').addClass('on')
+            : $('.header').removeClass('on');
+    })
+
+})
